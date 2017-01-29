cdd1a6faf4a235061db848afed1f7c6f1fb1a089
Yang Song <songyangustc@gmail.com>
2017-01-29 22:30:47 +0800

    Update Base64Encoding.java

--- Base64Encoding.java
+++ Base64Encoding.java
@@ -5,33 +5,97 @@
  
 public class Base64Encoding {
     
-    static String encodeString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-    
-    public char[] encode(byte[] byteArray) {
-        int length = byteArray.length % 3 == 0 ? byteArray.length / 3 * 4 : (byteArray.length / 3 + 1) * 4;
-        char[] result = new byte[length];
-        
-        for (int i = 0; i < byteArray.length / 3; i++) {
-            result[4 * i] = encodeString.charAt(byteArray[3 * i] >> 2);
-            result[4 * i + 1] = encodeString.charAt(((byteArray[3 * i] & 0b11) << 4) & (byteArray[3 * i + 1] >> 4));
-            result[4 * i + 2] = encodeString.charAt(((byteArray[3 * i + 1] & 0b1111) << 2) & (byteArray[3 * i + 2] >> 6));
-            result[4 * i + 3] = encodeString.charAt(byteArray[3 * i + 2] & 0b111111);
+{
+    public static String encode(byte[] data)
+    {
+        char[] tbl = {
+            'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+            'Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f',
+            'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v',
+            'w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/' };
+
+        StringBuilder buffer = new StringBuilder();
+        int pad = 0;
+        for (int i = 0; i < data.length; i += 3) {
+
+            int b = ((data[i] & 0xFF) << 16) & 0xFFFFFF;
+            if (i + 1 < data.length) {
+                b |= (data[i+1] & 0xFF) << 8;
+            } else {
+                pad++;
+            }
+            if (i + 2 < data.length) {
+                b |= (data[i+2] & 0xFF);
+            } else {
+                pad++;
+            }
+
+            for (int j = 0; j < 4 - pad; j++) {
+                int c = (b & 0xFC0000) >> 18;
+                buffer.append(tbl[c]);
+                b <<= 6;
+            }
         }
-       
-        if (byteArray.length % 3 == 1) {
-            int index = byteArray.length / 3 * 3 + 1;
-            result[byteArray.length / 3 * 4 + 1] = encodeString.charAt(byteArray[index] >> 2);
-            result[byteArray.length / 3 * 4 + 2] = encodeString.charAt((byteArray[index] & 0b11) << 4);
-            result[byteArray.length / 3 * 4 + 3] = '=';
-            result[byteArray.length / 3 * 4 + 4] = '=';
-        } else if (byteArray.length % 3 == 2) {
-            int index = byteArray.length / 3 * 3 + 1;
-            result[byteArray.length / 3 * 4 + 1] = encodeString.charAt(byteArray[index] >> 2);
-            result[byteArray.length / 3 * 4 + 2] = encodeString.charAt((byteArray[index] & 0b11) << 4) & (byteArray[index + 1] >> 4));
-            result[byteArray.length / 3 * 4 + 3] = encodeString.charAt(byteArray[index + 1] & 0b1111) << 2);
-            result[byteArray.length / 3 * 4 + 4] = '=';
+        for (int j = 0; j < pad; j++) {
+            buffer.append("=");
         }
-        
-        return result;
+
+        return buffer.toString();
+    }
+
+    public static byte[] decode(String data)
+    {
+        int[] tbl = {
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54,
+            55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2,
+            3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30,
+            31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+            48, 49, 50, 51, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+        byte[] bytes = data.getBytes();
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        for (int i = 0; i < bytes.length; ) {
+            int b = 0;
+            if (tbl[bytes[i]] != -1) {
+                b = (tbl[bytes[i]] & 0xFF) << 18;
+            }
+            // skip unknown characters
+            else {
+                i++;
+                continue;
+            }
+
+            int num = 0;
+            if (i + 1 < bytes.length && tbl[bytes[i+1]] != -1) {
+                b = b | ((tbl[bytes[i+1]] & 0xFF) << 12);
+                num++;
+            }
+            if (i + 2 < bytes.length && tbl[bytes[i+2]] != -1) {
+                b = b | ((tbl[bytes[i+2]] & 0xFF) << 6);
+                num++;
+            }
+            if (i + 3 < bytes.length && tbl[bytes[i+3]] != -1) {
+                b = b | (tbl[bytes[i+3]] & 0xFF);
+                num++;
+            }
+
+            while (num > 0) {
+                int c = (b & 0xFF0000) >> 16;
+                buffer.write((char)c);
+                b <<= 8;
+                num--;
+            }
+            i += 4;
+        }
+        return buffer.toByteArray();
     }
 }
