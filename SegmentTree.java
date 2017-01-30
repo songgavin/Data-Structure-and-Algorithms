f2917e5b7328a6d6d314f5598e5c0d34c3351b94
Yang Song <songyangustc@gmail.com>
2017-01-30 18:42:02 +0800

    Create SegmentTree.java

---
+++ SegmentTree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition of SegmentTreeNode:
+ * public class SegmentTreeNode {
+ *     public int start, end;
+ *     public SegmentTreeNode left, right;
+ *     public SegmentTreeNode(int start, int end) {
+ *         this.start = start, this.end = end;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class SegmentTree {
+    /**
+     *@param start, end: Denote an segment / interval
+     *@return: The root of Segment Tree
+     */
+    public SegmentTreeNode build(int start, int end) {
+        // write your code here
+        if (start > end) {
+            return null;
+        }
+        if (start == end) {
+            return new SegmentTreeNode(start, end);
+        }
+        
+        SegmentTreeNode root = new SegmentTreeNode(start, end);
+        int mid = start + (end - start) / 2;
+        root.left = build(start, mid);
+        root.right = build(mid + 1, end);
+        return root;
+    }
+}
