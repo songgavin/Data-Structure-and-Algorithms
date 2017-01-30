1098b9f912df91d83496f0121f28cc230a606b0c
Yang Song <songyangustc@gmail.com>
2017-01-30 19:47:11 +0800

    Update SegmentTree2.java

--- SegmentTree2.java
+++ SegmentTree2.java
@@ -38,4 +38,23 @@ public class SegmentTree2 {
         root.right = right;
         return root;
     }
+    
+    /*
+     *@param root, start, end: The root of segment tree and an segment / interval
+     *@return: The maximum number in the interval [start, end]
+     */
+    public int query(SegmentTreeNode root, int start, int end) {
+        // write your code here
+        if (root.start == start && root.end == end) {
+            return root.max;
+        }
+        int mid = root.start + (root.end - root.start) / 2;
+        if (end <= mid) {
+            return query(root.left, start, end);
+        } else if (start > mid) {
+            return query(root.right, start, end);
+        } else {
+            return Math.max(query(root.left, start, mid), query(root.right, mid + 1,end));
+        }
+    }
 }
