a5798ab0a6e4df522d49db1d5b00ff25b9017d44
Yang Song <songyangustc@gmail.com>
2017-02-07 16:35:09 -0800

    Create CountNumberIterator.java

---
+++ CountNumberIterator.java
@@ -0,0 +1,38 @@
+// fourth question in http://www.1point3acres.com/bbs/thread-217004-1-1.html
+public class CountNumberIterator {
+
+    int[] nums;
+    int count;
+    int index;
+
+    public CountNumberIterator(int[] nums) {
+        this.nums = nums;
+        count = 0;
+        index = 0;
+    }
+
+    public int next() {
+        count++;
+        return nums[index + 1];
+    }
+
+    public boolean hasNext() {
+        while (index < nums.length && count == nums[index] ) {
+            count = 0;
+            index += 2;
+        }
+        if (index >= nums.length) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public static void main(String[] args) {
+        CountNumberIterator c = new CountNumberIterator(new int[]{2, 3, 0, 4});
+        while (c.hasNext()) {
+            System.out.println(c.next());
+        }
+    }
+}
+
